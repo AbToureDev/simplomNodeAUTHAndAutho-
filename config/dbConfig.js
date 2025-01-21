fa566c73b4bbe4b192fe7d6d8bbf8f0ff88ae0b7
toure <aboubacartour993@gmail.com>
2025-01-21 16:02:37 +0100

    ajout de la securite

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -5,16 +5,10 @@ const mongoUri = process.env.LOCAL_CONNECT;
 
 
 mongoose.connect(mongoUri).then(r  => {
-    // console.log('connection a la base de donne.')
+    console.log('connection a la base de donne.')
 });
 if (!mongoUri) {
     console.error("Erreur : La variable d'environnement LOCAL_CONNECT n'est pas définie.");
     process.exit(1); // Quitte l'application si la chaîne de connexion est indéfinie
 }
-// mongoose.connection.on('connected', ()=> {
-//     console.log("connection a la base de donne.")
-// });
-// mongoose.connection.on('error', (error)=> {
-//     console.log("connection error:", error)
-// });
 module.exports =mongoose;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const {getAllUsers,getUserById, updateUser, deleteUser} = require('../controllers/userController');
 const verifyToken = require('../middleware/authMiddleware');
-router.get('/users', verifyToken, getAllUsers)
-router.get('/:id', getUserById)
+router.get('/users', getAllUsers)
+router.get('/:id',verifyToken, getUserById)
 router.get('/:id', updateUser)
 router.get('/:id', deleteUser)
 module.exports = router;
